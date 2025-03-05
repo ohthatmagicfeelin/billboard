f7819e4906c41c9a5bdb651008d6f78d0df83b99
ohthatmagicfeelin <balancedirect@gmail.com>
2025-03-05 12:13:22 +1100

    refactor(api : billboard): Simplify billboard API calls using centralized api utility

--- client/src/features/billboard/api/billboardApi.js
+++ client/src/features/billboard/api/billboardApi.js
@@ -1,11 +1,8 @@
-import axios from 'axios';
-import config from '@/config/env';
-
-const BILLBOARD_API = `${config.BACKEND_URL}/api/billboard`;
+import api from '@/api/api';
 
 export const billboardApi = {
   getHistoricalWeek: async (year, currentDate) => {
-    const response = await axios.get(`${BILLBOARD_API}/historical-week`, {
+    const response = await api.get('/api/billboard/historical-week', {
       params: {
         year,
         currentDate
